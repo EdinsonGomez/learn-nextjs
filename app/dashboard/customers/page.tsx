cf9679a5ec388bfc4857798e0a6efe266dea1e52
EdinsonGomez <edinsongomez92@gmail.com>
2024-02-01 23:44:58 -0500

    Chapter 1 - 5 done

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+function page() {
+  return (
+    <h2>Customers Page</h2>
+  )
+}
+
+export default page
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+function page() {
+  return (
+    <h2>Invoices Page</h2>
+  )
+}
+
+export default page
